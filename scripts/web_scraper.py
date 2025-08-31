83a6a0c3ffff11ef3f9c4f20f66891a63a36a3ba
Tanya Charan <tanyacharan58@gmail.com>
2025-08-31 14:22:19 -0700

    Fixed the web_scraper.py to get CNBC page and save the HTML

--- scripts/web_scraper.py
+++ scripts/web_scraper.py
@@ -4,14 +4,26 @@ from bs4 import BeautifulSoup
 #URL
 url= "https://www.cnbc.com/world/?region=world"
 
+#browser
+headers = {
+	"User-Agent": (
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+		"AppleWebKit/537.36 (KHTML, like Gecko) "
+		"Chrome/119.0.0.0 Safari/537.36"
+	)
+}
+
 #send request
-response = requests.get(url)
+response = requests.get(url, headers=headers)
 
 #save raw html
-raw_file = "../data/raw_data/web_data.html"
-with open(raw_file, "w", encoding="utf-8") as p:
-	p.write(response.text)
-print("Web data saved here", raw_file)
+if response.status_code == 200:
+	raw_file = "../data/raw_data/web_data.html"
+	with open(raw_file, "w", encoding="utf-8") as p:
+		p.write(response.text)
+	print("Web data saved here", raw_file)
+else:
+	print("Failed to get page, status:", response.status_code)
 
 soup= BeautifulSoup(response.text, "html.parser")
 
